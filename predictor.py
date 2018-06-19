231b3f161154a4e818cf82abc776acf1975be22a
madewitt <martin.dewitt@gmail.com>
2018-06-19 14:22:07 -0400

    Removed debugging print statements and added comments.

--- predictor.py
+++ predictor.py
@@ -1,97 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+A simple temperature predictor for Fort Lauderdale Florida.
 """
 from matplotlib import pyplot as plt
 import numpy as np
 
-weather_filename = 'weatherdata.csv'
-weather_file = open(weather_filename)
-weather_data = weather_file.read()
-weather_file.close()
-
-
-# print(weather_data[:200])
-
-# Break the weather data string into lines
-lines = weather_data.split('\n')
-# print(lines[:2])
-
-# Strip of the first line as labels and the rest of the lines as
-# the data values
-labels = lines[0]
-values = lines[1:]
-n_values = len(values)
-
-year = []
-month = []
-day = []
-max_temp = []
-j_year = 1
-j_month = 2
-j_day = 3
-j_max_temp = 5
-
-for i_row in range(n_values):
-    split_values = values[i_row].split(',')
-    if len(split_values) >= j_max_temp:
-        year.append(int(split_values[j_year]))
-        month.append(int(split_values[j_month]))
-        day.append(int(split_values[j_day]))
-        max_temp.append(float(split_values[j_max_temp]))
-
-
-# start_val = 19560
-# end_val = start_val+31
-# print(split_values[start_val:end_val],'\n')
-# print(year[start_val:end_val],'\n')
-# print(month[start_val:end_val],'\n')
-# print(day[start_val:end_val],'\n')
-# print(max_temp[start_val:end_val],'\n')
-#
-# plt.scatter(range(len(max_temp)),max_temp)
-
-
-# Isolate the recent data
-i_mid = len(max_temp) // 2
-temps = np.array(max_temp[i_mid:])
-temps[np.where(temps == -99.9)] = np.nan
-year = np.array(year[i_mid:])
-month = np.array(month[i_mid:])
-day = np.array(day[i_mid:])
-
-
-# print(np.where(np.isnan(temps))[0])
-# print(np.where(np.logical_not(np.isnan(temps)))[0][0])
-
-# Start the data where the large number of consecutive nans stops
-i_start = np.where(np.logical_not(np.isnan(temps)))[0][0]
-temps = temps[i_start:]
-year = year[i_start:]
-month = month[i_start:]
-day = day[i_start:]
-
-b = np.isnan(temps)
-i_nans = np.where(np.isnan(temps))[0]
-# print(i_nans)
-
-# Replace all nans with the most recent non-nan value
-for i in i_nans:
-    temps[i] = temps[i-1]
-
-# print(temps[b])
-
-# Determine whether the previous day's temperature is
-# related to that of today.
-
-# plt.plot(temps[:-1],temps[1:],color='black',marker='.',linestyle='None')
-# plt.show()
-
-
 # Show the relationship between two variables adding some normally
 # distributed jitter
+
+
 def scatter_with_jitter(x, y):
     '''
     Make a scatter plot with jitter
@@ -106,21 +23,6 @@ def scatter_with_jitter(x, y):
         alpha=0.1
     )
     plt.show()
-
-
-# shift = 1
-# scatter_with_jitter(temps[:-shift],temps[shift:])
-autocorr = []
-for shift in range(1, 1000):
-    correlation = np.corrcoef(temps[:-shift], temps[shift:])[0, 1]
-    autocorr.append(correlation)
-
-# Add a sine wave approximation to fit autocorr for up to 1000 day shifts
-d = np.arange(1000)
-fit = 0.6*np.sin((2*np.pi/365)*(d+91.25))
-# plt.plot(d,fit,color='green')
-# plt.plot(autocorr)
-# plt.show()
 
 
 def find_day_of_year(year, month, day):
@@ -146,17 +48,82 @@ def find_day_of_year(year, month, day):
     return np.sum(days_per_month[0:month-1]) + day - 1
 
 
+# Import data from the csv file
+weather_filename = 'weatherdata.csv'
+weather_file = open(weather_filename)
+weather_data = weather_file.read()
+weather_file.close()
+
+# Break the weather data string into lines
+lines = weather_data.split('\n')
+
+# Store the first line as labels and the rest of the lines as
+# the data values
+labels = lines[0]
+values = lines[1:]
+n_values = len(values)
+
+# Initialize lists for each relevant data field.
+year = []
+month = []
+day = []
+max_temp = []
+
+# Define index variables for each data column in 'values'.
+j_year = 1
+j_month = 2
+j_day = 3
+j_max_temp = 5
+
+# Split up data values using a comma delimiter and store each value
+# in its corresponding list.
+for i_row in range(n_values):
+    split_values = values[i_row].split(',')
+    if len(split_values) >= j_max_temp:
+        year.append(int(split_values[j_year]))
+        month.append(int(split_values[j_month]))
+        day.append(int(split_values[j_day]))
+        max_temp.append(float(split_values[j_max_temp]))
+
+
+# Since there is a significant amount of missing data early on in the
+# dataset, select out only the recent half of the data.
+i_mid = len(max_temp) // 2
+temps = np.array(max_temp[i_mid:])
+year = np.array(year[i_mid:])
+month = np.array(month[i_mid:])
+day = np.array(day[i_mid:])
+# Replace Florida State University's missing data temperature value of
+# -99.9 with nan
+temps[np.where(temps == -99.9)] = np.nan
+
+
+# There is still a large number of nans in the temperature field. Eliminate
+# these from the dataset.
+# Find the first index after the large number of consecutive nans.
+i_start = np.where(np.logical_not(np.isnan(temps)))[0][0]
+
+# Slice the data starting at the first non-nan temperature index.
+temps = temps[i_start:]
+year = year[i_start:]
+month = month[i_start:]
+day = day[i_start:]
+
+
+# Get the indices of the remaining nan values in the temperature array.
+i_nans = np.where(np.isnan(temps))[0]
+
+# Replace all remaining sparse nans in the temperature array with the
+# temperature for the preceeding day.
+for i in i_nans:
+    temps[i] = temps[i-1]
+
+
 day_of_year = np.zeros(temps.size)
 for i_row in range(temps.size):
     day_of_year[i_row] = find_day_of_year(
         year[i_row], month[i_row], day[i_row])
 
-# print(year[:10])
-# print(month[:10])
-# print(day[:10])
-# print(day_of_year[:10])
-
-# scatter_with_jitter(day_of_year,temps)
 
 # Create 10 day medians for each day of the year
 median_temp_calendar = np.zeros(366)
@@ -179,10 +146,3 @@ for i_day in range(0, 365):
     if i_day == 364:
         ten_day_medians[np.where(day_of_year == 365)] = ten_day_median
         median_temp_calendar[365] = ten_day_median
-
-# plt.plot(np.arange(temps.size),ten_day_medians,marker='.',linestyle='None')
-# plt.show()
-# plt.plot(temps)
-# plt.plot(median_temp_calendar)
-plt.plot(ten_day_medians)
-plt.show()
